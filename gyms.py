dbcf6e06bdc526f51f17062b243ad58094f08f3d
Sonny Parlin <sonny@graciebradenton.com>
2018-10-06 10:29:42 -0400

    Initial commit

---
+++ gyms.py
@@ -0,0 +1,56 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import pandas as pd
+
+# mapbox_access_token = 'ADD_YOUR_TOKEN_HERE'
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/Nuclear%20Waste%20Sites%20on%20American%20Campuses.csv')
+site_lat = df.lat
+site_lon = df.lon
+locations_name = df.text
+
+data = [
+    go.Scattermapbox(
+        lat=site_lat,
+        lon=site_lon,
+        mode='markers',
+        marker=dict(
+            size=17,
+            color='rgb(255, 0, 0)',
+            opacity=0.7
+        ),
+        text=locations_name,
+        hoverinfo='text'
+    ),
+    go.Scattermapbox(
+        lat=site_lat,
+        lon=site_lon,
+        mode='markers',
+        marker=dict(
+            size=8,
+            color='rgb(242, 177, 172)',
+            opacity=0.7
+        ),
+        hoverinfo='none'
+    )]
+
+layout = go.Layout(
+    title='Nuclear Waste Sites on Campus',
+    autosize=True,
+    hovermode='closest',
+    showlegend=False,
+    mapbox=dict(
+        bearing=0,
+        center=dict(
+            lat=38,
+            lon=-94
+        ),
+        pitch=0,
+        zoom=3,
+        style='light'
+    ),
+)
+
+fig = dict(data=data, layout=layout)
+py.plot(fig, filename='Nuclear Waste Sites on American Campuses')
