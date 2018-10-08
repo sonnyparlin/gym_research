465ee94fcd8317445063d5674f89291bc9ff1bf5
Sonny Parlin <sonny@graciebradenton.com>
2018-10-08 18:28:08 -0400

    Renamed some files for clarity

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from apps import app1, app2
+from apps import bjjmap, app2
 
 
 app.layout = html.Div([
@@ -16,12 +16,12 @@ app.layout = html.Div([
     Output('page-content', 'children'), 
     [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/app1':
-         return app1.layout
+    if pathname == '/apps/bjjmap':
+         return bjjmap.layout
     elif pathname == '/apps/app2':
          return app2.layout
     else:
-        return app1.layout # return '404'
+        return bjjmap.layout # return '404'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
