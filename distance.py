c61139ab69c0a907ff71d8e8b7b072472e1a5010
Sonny Parlin <sonny@graciebradenton.com>
2018-10-08 03:59:01 -0400

    Finally got this fucker working

--- distance.py
+++ distance.py
@@ -1,42 +1,7 @@
-from math import cos, asin, sqrt
-import pandas as pd
-
+import geopy.distance
 
 def distance(lat1, lon1, lat2, lon2):
-    p = 0.017453292519943295     #Pi/180
-    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
-    return 12742 * asin(sqrt(a))
+    coords_1 = (lat1, lon1)
+    coords_2 = (lat2, lon2)
 
-
-def build_distance_tuples(p=False):
-    df = pd.read_csv('https://raw.githubusercontent.com/sonnyparlin/gym_research/master/rkbjj_florida.csv')
-    site_lat = df.lat
-    site_lon = df.lon
-    locations_name = df.text
-    dh={}
-    final=[]
-    last=0
-    for i in range(len(site_lat)):
-        lat1=site_lat[i]
-        lon1=site_lon[i]
-        dist_array=[]
-    
-        for x in range(len(site_lat -1)):
-            if site_lat[x] == site_lat[i]:
-                continue
-            else:
-                lat2=site_lat[x]
-                lon2=site_lon[x]
-        
-            dist = int(distance(lat1, lon1, lat2, lon2) / 1.609)
-            if (dist < 20) and (locations_name[x],locations_name[i], dist) not in final:
-                final.append((locations_name[i],locations_name[x], dist))
-
-    final = list(set(final))
-    final = sorted(final, key=lambda x: x[2])
-    if p:
-        for a, b, c in final:
-           print("{} is {} miles from {}".format(a,c,b))
-    return final
-    
-#final = build_distance_tuples()
+    return geopy.distance.vincenty(coords_1, coords_2).miles
--- gyms.py
+++ gyms.py
@@ -1,49 +1,75 @@
-import plotly.plotly as py
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.graph_objs as go
-import distance
 import pandas as pd
+import distance
 
-# mapbox_access_token = 'ADD_YOUR_TOKEN_HERE'
+app = dash.Dash()
 
 df = pd.read_csv('https://raw.githubusercontent.com/sonnyparlin/gym_research/master/rkbjj_florida.csv')
-site_lat = df.lat
-site_lon = df.lon
-locations_name = df.text
-dist = distance.build_distance_tuples(True)
     
-trace = go.Scattermapbox(
-        lat=site_lat,
-        lon=site_lon,
-        mode='markers+text',
-        textposition='middle right',
-        marker=dict(
-            size=17,
-            color='rgb(0, 128, 0)',
-            opacity=0.7
-        ),
-        text=locations_name,
-        hoverinfo='text'
+#print(dist_hash)
+
+app.layout = html.Div([
+    html.H1('RKBJJ Gyms'),
+    html.Div(id='text-content'),
+    dcc.Graph(id='map', figure={
+        'data': [{
+            'lat': df['lat'],
+            'lon': df['lon'],
+            'mode':'markers+text',
+            'textposition':'middle right',
+            'marker': {
+                'color': 'rgb(0, 128, 0)',
+                'size': 8,
+                'opacity': 0.6
+            },
+            'text': df['gyms'],
+            'type': 'scattermapbox'
+        }],
+        'layout': {
+            'mapbox': {
+                'accesstoken': 'pk.eyJ1IjoiY2hyaWRkeXAiLCJhIjoiY2ozcGI1MTZ3MDBpcTJ3cXR4b3owdDQwaCJ9.8jpMunbKjdq1anXwU5gxIw',
+                'bearing':0,
+                'center'  : {
+                        'lat':'27.849172',
+                        'lon':'-82.234936'
+                    },
+                'pitch':20,
+                'zoom':8.5,
+                'style':'light'
+            },
+            'hovermode': 'closest',
+            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
+        }
+    })
+])
+
+@app.callback(
+    dash.dependencies.Output('text-content', 'children'),
+    [dash.dependencies.Input('map', 'selectedData')])
+def update_text(selectedData):
+    print(selectedData)
+    s=""
+    di = ""
+    try:
+        lat1 = selectedData['points'][0]['lat']
+        lon1 = selectedData['points'][0]['lon']
+        lat2 = selectedData['points'][1]['lat']
+        lon2 = selectedData['points'][1]['lon']
+        di = distance.distance(lat1, lon1, lat2, lon2)
+    except TypeError:
+        pass
+    comp=""
+    
+    return html.H3(
+        '{} is {:.1f} miles away from {}'.format(selectedData['points'][0]['text'], di, selectedData['points'][1]['text'])
     )
 
-layout = go.Layout(
-    title='RKBJJ Gyms in Florida',
-    autosize=False,
-    width=1000,
-    height=700,
-    showlegend=False,
-    hovermode='closest',
-    mapbox=dict(
-        bearing=0,
-        center=dict(
-            lat=27.849172,
-            lon=-82.234936
-        ),
-        pitch=20,
-        zoom=8.5,
-        style='light'
-    ),
-)
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
 
-data = [trace]
-fig = dict(data=data, layout=layout)
-py.plot(fig, filename='RKBJJ Schools')
+if __name__ == '__main__':
+    app.run_server(debug=True)
