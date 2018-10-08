526cc6dcb647c12026ce3e9a3aeb87f7ec7418f9
Sonny Parlin <sonny@graciebradenton.com>
2018-10-08 18:16:35 -0400

    Turned this into a proper dash app with file structure

--- distance.py
+++
@@ -1,7 +0,0 @@
-import geopy.distance
-
-def distance(lat1, lon1, lat2, lon2):
-    coords_1 = (lat1, lon1)
-    coords_2 = (lat2, lon2)
-
-    return geopy.distance.vincenty(coords_1, coords_2).miles
