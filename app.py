f662167ea755ff54ccf7e8c82793a55bde42bb2b
Sonny Parlin <sonny@graciebradenton.com>
2018-10-14 19:30:25 -0400

    Moved contents of index.py to app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from apps import bjjmap
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,3 +14,24 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets,
     }])
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.layout = html.Div([ 
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content') 
+])
+
+app.title="BJJ Map"
+
+@app.callback(
+    Output('page-content', 'children'), 
+    [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/apps/bjjmap':
+         return bjjmap.layout
+    elif pathname == '/apps/app2':
+         return app2.layout
+    else:
+        return bjjmap.layout # return '404'
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
